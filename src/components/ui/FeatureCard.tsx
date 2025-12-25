ab8b4d159ed40f70f55ed62ad4a67850da7a224b
HebaElgohary <hebasamir535@gmail.com>
2025-12-25 21:47:39 +0200

    fix subtitle color

--- src/components/ui/FeatureCard.tsx
+++ src/components/ui/FeatureCard.tsx
@@ -12,7 +12,7 @@ export default function FeatureCard({icon,title,subtitle}:FeatureCardProps) {
       <div className="icon my-2">{icon}</div> 
       <div className="title text-primary font-extrabold lg:text-2xl my-2 text-center
 " >{title}</div>
-        <div className="subtitle text-center text-[#4F4F4F]">{subtitle}</div>
+        <div className="subtitle text-center text-secondary">{subtitle}</div>
     </div>
   )
 }
